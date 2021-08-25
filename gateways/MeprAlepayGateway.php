199eb8f496de95dc9eef6944400ccfd22cff97da
DalatCoder <hieuntctk42@gmail.com>
2021-08-25 12:12:47 +0700

    Fix subscription expired time

--- gateways/MeprAlepayGateway.php
+++ gateways/MeprAlepayGateway.php
@@ -361,6 +361,7 @@ class MeprAlepayGateway extends MeprBaseRealGateway
         $sub->created_at = gmdate('c');
         $sub->store();
 
+        /**
         // If trial amount is zero then we've got to make sure the confirmation txn lasts through the trial
         if ($sub->trial && $sub->trial_amount <= 0.00) {
             $expires_at = MeprUtils::ts_to_mysql_date(time() + MeprUtils::days($sub->trial_days), 'Y-m-d 23:59:59');
@@ -369,11 +370,12 @@ class MeprAlepayGateway extends MeprBaseRealGateway
         } else {
             $expires_at = $txn->created_at; // Expire immediately
         }
+        */
 
         $txn->trans_num = $sub->subscr_id;
-        $txn->status = MeprTransaction::$confirmed_str;
+        $txn->status = MeprTransaction::$complete_str;
         $txn->txn_type = MeprTransaction::$subscription_confirmation_str;
-        $txn->expires_at = $expires_at;
+        // $txn->expires_at = $expires_at;
         $txn->set_subtotal(0.0);
         error_log('Mepre Transaction Store ' . print_r($txn, true));
         $txn->store();
